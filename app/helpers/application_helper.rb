8f5052595df3a176f415c9a420713cfd38628065
synupsk <synupsk@gmail.com>
2015-07-26 14:09:47 +0530

    Adding helper method to title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Helper method to render title
+  def title 
+    base_title = "Sample App Pages"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
 end
