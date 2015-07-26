ac48a74a763d4474470a60797113cd7257a78f9f
synupsk <synupsk@gmail.com>
2015-07-26 13:22:59 +0530

    Adding Common title

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = 'Home'
   end
 
   def help
     @helpText = 'Hey I\'m a single quote string'
     @otherHelpText = "Hey I'm double quote string"
-    
+    @title = 'Help'
   end
 end
